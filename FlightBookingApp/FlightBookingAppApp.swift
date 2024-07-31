646ef750575f7238e49d3dd04afbf03d53dd92aa
ahmetakay24 <ahmetakay6600@gmail.com>
2024-07-31 20:42:15 +0300

    Initial Commit

---
+++ FlightBookingApp/FlightBookingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightBookingAppApp.swift
+//  FlightBookingApp
+//
+//  Created by TheHustleMachine on 31.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct FlightBookingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
